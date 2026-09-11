76bc039e9f2bec1c496d820fd6f66b9502f15a0e
HellMind-azx <nostalgiabook176@gmail.com>
2026-09-11 17:14:26 +0500

    mobile menu is done

--- corneli-app/js/burger-menu.js
+++ corneli-app/js/burger-menu.js
@@ -1,8 +1,17 @@
 const burgerMenu = document.querySelector('.header__burger-menu');
+const close = document.querySelector('.burger-menu--active');
 const menu = document.querySelector('.header__link-container');
+const backToTopButton = document.querySelector('.to-top');
 
 burgerMenu.addEventListener('click', () => {
     burgerMenu.classList.toggle('burger-menu--active');
     menu.classList.toggle('is-active')
     document.body.classList.toggle('menu-open');
 });
+
+backToTopButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+})
