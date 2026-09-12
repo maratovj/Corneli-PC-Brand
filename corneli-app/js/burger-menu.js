73353463a96533fd1f23013d4e25ee12aa940d68
HellMind-azx <nostalgiabook176@gmail.com>
2026-09-12 15:35:22 +0500

    Mobile navbar style is a glassmorphism

--- corneli-app/js/burger-menu.js
+++ corneli-app/js/burger-menu.js
@@ -2,9 +2,11 @@ const burgerMenu = document.querySelector('.header__burger-menu');
 const close = document.querySelector('.burger-menu--active');
 const menu = document.querySelector('.header__link-container');
 const backToTopButton = document.querySelector('.to-top');
+const overlay = document.querySelector('.overlay')
 
 burgerMenu.addEventListener('click', () => {
     burgerMenu.classList.toggle('burger-menu--active');
+    overlay.classList.toggle('overlay--active');
     menu.classList.toggle('is-active')
     document.body.classList.toggle('menu-open');
 });
