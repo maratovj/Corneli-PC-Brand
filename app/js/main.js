3bccb60b166afdaf884386dfb6deccc00bc6c91a
HellMind-azx <nostalgiabook176@gmail.com>
2026-09-22 15:32:07 +0500

    removed optional chaining

--- app/js/main.js
+++ app/js/main.js
@@ -3,5 +3,5 @@ import { toggleClass, backToTop } from './burger-menu.js'
 const burgerMenu = document.querySelector('.header__burger-menu');
 const backToTopButton = document.querySelector('.to-top');
 
-burgerMenu?.addEventListener('click', toggleClass);
-backToTopButton?.addEventListener('click', backToTop);
+burgerMenu.addEventListener('click', toggleClass);
+backToTopButton.addEventListener('click', backToTop);
